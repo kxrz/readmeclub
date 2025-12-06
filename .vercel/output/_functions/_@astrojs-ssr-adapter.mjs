76543d0d71697cc7b34a14d3637e338f87ed1449
Florent B. <kxrz@users.noreply.github.com>
2025-12-06 18:40:53 +0100

    buldin

--- .vercel/output/_functions/_@astrojs-ssr-adapter.mjs
+++
@@ -1,1 +0,0 @@
-export { c as createExports, a as start } from './chunks/_@astrojs-ssr-adapter_D1nQ2Lln.mjs';
--- .vercel/output/_functions/chunks/client_DiYgajIf.mjs
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = "https://ykwwrqxcfsahcelqgzrs.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlrd3dycXhjZnNhaGNlbHFnenJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ5OTI0NzYsImV4cCI6MjA4MDU2ODQ3Nn0.tfMJDZ9lkzJhJgZGUoU2Xjy-wkWGd0adC2cSL0PJ84o";
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
-export { supabase as s };
